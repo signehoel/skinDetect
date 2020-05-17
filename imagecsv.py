b5e01cd47149f0fad82f06be064a10fadfded3c9
Signe Hoel <signe.hoel@gmail.com>
2020-05-17 09:46:29 -0400

    Add files via upload

---
+++ imagecsv.py
@@ -0,0 +1,31 @@
+import os
+
+benigntestdirectory = r'skin-cancer-malignant-vs-benign/test/benign'
+malignanttestdirectory = r'skin-cancer-malignant-vs-benign/test/malignant'
+
+benigntraindirectory = r'skin-cancer-malignant-vs-benign/train/benign'
+malignanttraindirectory = r'skin-cancer-malignant-vs-benign/train/malignant'
+
+testbenign = "gs://skin-lesion-classifier-277418-vcm/skin-cancer-malignant-vs-benign/test/benign/"
+testmalignant = "gs://skin-lesion-classifier-277418-vcm/skin-cancer-malignant-vs-benign/test/malignant/"
+
+trainbenign = "gs://skin-lesion-classifier-277418-vcm/skin-cancer-malignant-vs-benign/train/benign/"
+trainmalignant = "gs://skin-lesion-classifier-277418-vcm/skin-cancer-malignant-vs-benign/train/malignant/"
+
+for benigntest in os.listdir(benigntestdirectory):
+    print(testbenign + benigntest + ",benign")
+
+print()
+
+for malignanttest in os.listdir(malignanttestdirectory):
+    print(testmalignant + malignanttest + ",malignant")
+
+print()
+
+for benigntrain in os.listdir(benigntraindirectory):
+    print(trainbenign + benigntrain + ",benign")
+
+print()
+
+for malignanttrain in os.listdir(malignanttraindirectory):
+    print(trainmalignant + malignanttrain + ",malignant")
